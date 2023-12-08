c73e3a9e08a9be95a68e80f98b5d1d0915e85aa5
Navneet Pandey <navneet.pandey@teamcomputers.com>
2023-12-08 11:36:17 +0530

    added describe.skip in divide section

--- src/calculator.test.js
+++ src/calculator.test.js
@@ -113,7 +113,7 @@ describe('multiply', () => {
   });
 });
 
-describe('divide', () => {
+describe.skip('divide', () => {
   it('should throw a TypeError if arguments are not numbers', () => {
     expect(() => calculator.divide(40, '2')).to.throw(TypeError);
     expect(() => calculator.divide(40, [])).to.throw(TypeError);
